7012b01284f3c250f5e76501e24ab84580d7e8c2
Lejlana <lelabeganovic@gmail.com>
2023-04-06 20:35:08 +0200

    Update1

--- src/zadatak1/Student.java
+++ src/zadatak1/Student.java
@@ -1,2 +1,35 @@
-package zadatak1;public class Student {
+package zadatak1;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student (final String firstname, final String lastName, final int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName () {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
+
+
